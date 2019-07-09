73eacf69593b232441c9c85826de2088b0f61369
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-07-09 22:46:12 +0900

    練習問題3.3を回答

--- ch03/ex03/surface.go
+++ ch03/ex03/surface.go
@@ -12,6 +12,11 @@ const (
 	xyscale       = width / 2 / xyrange
 	zscale        = height * 0.4
 	angle         = math.Pi / 6
+
+	// sin(r) / r の最小と最大はそれぞれ -0.22, 1.0の近似値なのでmin, maxは計算せずに定数を置く
+	// https://www.wolframalpha.com/input/?i=min+max+sin((x%5E2%2By%5E2)%5E(1%2F2))+%2F+(x%5E2%2By%5E2)%5E(1%2F2),+0+%3C%3D+x+%3C%3D+30,+0+%3C%3D+y+%3C%3D+30
+	minZ = -0.22
+	maxZ = 1.0
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
@@ -28,10 +33,10 @@ func main() {
 
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
+			ax, ay, az := corner(i+1, j)
+			bx, by, bz := corner(i, j)
+			cx, cy, cz := corner(i, j+1)
+			dx, dy, dz := corner(i+1, j+1)
 
 			f := func() {
 				for _, point := range []float64{ax, ay, bx, by, cx, cy, dx, dy} {
@@ -39,7 +44,12 @@ func main() {
 						return
 					}
 				}
-				fmt.Printf("<polygon points='%g,%g %g,%g, %g,%g %g,%g' fill='url(#gradient)' />\n", ax, ay, bx, by, cx, cy, dx, dy)
+				z := (az + bz + cz + dz) / 4
+				// zを0〜1に正規化
+				nz := (z-minZ)/maxZ - minZ
+				red := int(nz * 255)
+				blue := 255 - red
+				fmt.Printf("<polygon points='%g,%g %g,%g, %g,%g %g,%g' fill='rgb(%d, 0, %d)' />\n", ax, ay, bx, by, cx, cy, dx, dy, red, blue)
 			}
 			f()
 		}
@@ -47,15 +57,15 @@ func main() {
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int) (sx float64, sy float64) {
+func corner(i, j int) (float64, float64, float64) {
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
 	z := f(x, y)
 
-	sx = width/2 + (x-y)*cos30*xyscale
-	sy = height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy, z
 }
 
 func f(x, y float64) float64 {
