03a9b0f13f384fb7b7c95d9b3f76782695310927
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-07-14 13:28:20 +0900

    練習問題3.7まで回答

--- ch03/ex07/newton.go
+++ ch03/ex07/newton.go
@@ -8,6 +8,8 @@ import (
 	"os"
 )
 
+const epsilon = 1e-5
+
 func main() {
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
@@ -19,10 +21,10 @@ func main() {
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
-			img.Set(px, py, mandelbrot(z))
+			img.Set(px, py, newton(z))
 		}
 	}
-	// nimg => newImage
+	// new image
 	nimg := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
 		for px := 0; px < width; px++ {
@@ -111,16 +113,37 @@ func main() {
 	png.Encode(os.Stdout, nimg)
 }
 
-func mandelbrot(z complex128) color.Color {
+func newton(z complex128) color.Color {
 	const iterations = 200
 	const contrast = 15
 
-	var v complex128
 	for n := uint8(0); n < iterations; n++ {
-		v = v*v + z
-		if cmplx.Abs(v) > 2 {
-			return color.YCbCr{Y: 255 - contrast*n, Cb: 255 - contrast*(n+1), Cr: 255 - contrast*(n+2)}
+		// ニュートン法のアルゴリズムは右記を参照. https://algorithm.joho.info/mathematics/newton-method-program/
+		// 漸化式 αn - f(αn) / df(αn)
+		z = z - f(z)/df(z)
+
+		// z^4 - 1 = 0のとりうる解は、z = ±1 または z = ±iなので
+		// それぞれの解とzの差が0近似値となったものを正解値とする
+		if cmplx.Abs(1-z) < epsilon {
+			return color.RGBA{R: contrast * n, G: 0, B: 0, A: 255}
+		} else if cmplx.Abs(-1-z) < epsilon {
+			return color.RGBA{R: 0, G: 0, B: contrast * n, A: 255}
+		} else if cmplx.Abs(1i-z) < epsilon {
+			return color.RGBA{R: 0, G: contrast * n, B: 0, A: 255}
+		} else if cmplx.Abs(-1i-z) < epsilon {
+			return color.RGBA{R: 0, G: contrast * n, B: contrast * n, A: 255}
 		}
 	}
 	return color.Black
+}
+
+// z^4 - 1 = 0を求める関数
+func f(x complex128) complex128 {
+	return x*x*x*x - 1
+}
+
+// 導関数
+// https://ja.wolframalpha.com/input/?i=z%5E4+-1%E3%81%AE%E5%B0%8E%E9%96%A2%E6%95%B0
+func df(x complex128) complex128 {
+	return 4 * x * x * x
 }
