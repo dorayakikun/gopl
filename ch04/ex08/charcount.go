680b749033b771f1b7ffaf6ca7b696d2224c7de2
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-07-15 18:28:53 +0900

    練習問題4.8まで回答

---
+++ ch04/ex08/charcount.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[rune]int)
+	var utflen [utf8.UTFMax + 1]int
+	invalid := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+	fmt.Printf("rune\tcount\ttype\t\n")
+	for c, n := range counts {
+		var t string
+		if unicode.IsLetter(c) {
+			t = "letter"
+		} else if unicode.IsDigit(c) {
+			t = "digit"
+		} else {
+			t = "other"
+		}
+		fmt.Printf("%q\t%d\t%s\n", c, n, t)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
