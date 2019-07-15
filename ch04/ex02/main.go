7f8c736293b1a41691a7d69aa913329fca364743
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-07-15 19:08:46 +0900

    出力の見栄えを調整

--- ch04/ex02/main.go
+++ ch04/ex02/main.go
@@ -24,11 +24,11 @@ func main() {
 		s := input.Text()
 		switch *mode {
 		case 0:
-			fmt.Printf("%q is %x(SHA256)\n", s, sha256.Sum256([]byte(s)))
+			fmt.Printf("%q is %x\t(SHA256)\n", s, sha256.Sum256([]byte(s)))
 		case 1:
-			fmt.Printf("%q is %x(SHA384)\n", s, sha512.Sum384([]byte(s)))
+			fmt.Printf("%q is %x\t(SHA384)\n", s, sha512.Sum384([]byte(s)))
 		case 2:
-			fmt.Printf("%q is %x(SHA512)\n", s, sha512.Sum512([]byte(s)))
+			fmt.Printf("%q is %x\t(SHA512)\n", s, sha512.Sum512([]byte(s)))
 		default:
 			fmt.Printf("mode is %d want 0-2\n", *mode)
 			os.Exit(2)
