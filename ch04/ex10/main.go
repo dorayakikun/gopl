7cff745c7c4d6558a87d5371f3f3a6d445196563
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-07-22 22:23:57 +0900

    練習問題4.10まで回答

--- ch04/ex10/main.go
+++ ch04/ex10/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,7 +14,23 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
+
+	dateRanges := make(map[string][]*github.Issue)
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+		duration := time.Since(item.CreatedAt).Hours()
+		if duration< 744 {
+			dateRanges["past month"] = append(dateRanges["past month"], item)
+		} else if duration < 8760 {
+			dateRanges["past year"] = append(dateRanges["past year"], item)
+		} else {
+			dateRanges["more old"] = append(dateRanges["more old"], item)
+		}
+	}
+
+	for k, v := range dateRanges {
+		fmt.Printf("[%s]\n", k)
+		for _, item := range v {
+			fmt.Printf("#%-5d %9.9s %.55s %s\n", item.Number, item.User.Login, item.Title, item.CreatedAt.Format(time.RFC3339))
+		}
 	}
 }
