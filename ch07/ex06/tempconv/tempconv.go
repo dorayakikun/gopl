0f42c9fceb709a0af8fb41850064fd88f1d1c207
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-08-17 19:36:16 +0900

    練習問題7.6まで回答

---
+++ ch07/ex06/tempconv/tempconv.go
@@ -0,0 +1,41 @@
+package tempconv
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9.0/5.0 + 32.0) }
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32.0) * 5.0 / 9.0) }
+func KToC(k Kelvin) Celsius { return Celsius(-273.15 + k)}
+
+func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
+
+type celsiusFlag struct{ Celsius }
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit) // no error check needed
+	switch unit {
+	case "C", "°C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	case "K", "°K":
+		f.Celsius = KToC(Kelvin(value))
+		return nil
+	}
+	return fmt.Errorf("invalid temperature %q", s)
+}
+
+func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
