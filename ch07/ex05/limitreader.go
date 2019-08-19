cc9f5a576655aceaf016d0f25bb8c76266f04dba
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-08-19 22:06:48 +0900

    fix

--- ch07/ex05/limitreader.go
+++ ch07/ex05/limitreader.go
@@ -2,28 +2,25 @@ package limitreader
 
 import "io"
 
-type Reader struct {
-	R io.Reader
-	N int64
+type reader struct {
+	r io.Reader
+	n int64
 }
 
-func (r *Reader) Read(b []byte) (n int, err error) {
-	if r.N <= 0 {
+func (r *reader) Read(b []byte) (n int, err error) {
+	if r.n <= 0 {
 		err = io.EOF
 		return
 	}
-	if int64(len(b)) > r.N {
-		b = b[0:r.N]
+	if int64(len(b)) > r.n {
+		b = b[:r.n]
 	}
-	n, err = r.R.Read(b)
-	if err != nil {
-		return
-	}
-	r.N -= int64(n)
+
+	n, err = r.r.Read(b)
+	r.n -= int64(n)
 	return
 }
 
-func LimitReader(r io.Reader, n int64) (io.Reader, *int64) {
-	l := &Reader{r, n}
-	return l, &l.N
+func LimitReader(r io.Reader, n int64) (io.Reader) {
+	return &reader{r, n}
 }
--- ch07/ex05/limitreader_test.go
+++ ch07/ex05/limitreader_test.go
@@ -1,7 +1,6 @@
 package limitreader
 
 import (
-	"io"
 	"strings"
 	"testing"
 )
@@ -10,7 +9,7 @@ func TestReader_Read(t *testing.T) {
 	cases := []struct {
 		s string
 		n int64
-		want int64
+		want int
 	} {
 		{
 			s: "",
@@ -20,32 +19,27 @@ func TestReader_Read(t *testing.T) {
 		{
 			s: "a",
 			n: 1,
-			want: 0,
+			want: 1,
 		},
 		{
 			s: "abcdefg",
 			n: 3,
-			want: 0,
+			want: 3,
 		},
 		{
 			s: "あいうえお",
-			n: 200,
-			want: 185,
+			n: 9,
+			want: 9,
 		},
 	}
 
 	for _, c := range cases {
-		r, n := LimitReader(strings.NewReader(c.s), c.n)
-		p := make([]byte, 100)
-		for {
-			_, err :=r.Read(p)
-			if err == io.EOF {
-				break
-			}
-		}
+		r := LimitReader(strings.NewReader(c.s), c.n)
+		p := make([]byte, 10)
 
-		if *n != c.want {
-			t.Fatalf("s: %s n: %d want: %d\n", c.s, n, c.want)
+		n, _ := r.Read(p)
+		if n != c.want {
+			t.Fatalf("n: %d want: %d\n", n, c.want)
 		}
 	}
 }
