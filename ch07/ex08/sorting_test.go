e69fb2555fc922bb0786dba75ccdb0c9f753efe9
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-08-19 22:50:52 +0900

    fix

--- ch07/ex08/sorting_test.go
+++ ch07/ex08/sorting_test.go
@@ -1,45 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 	"testing"
+	"time"
 )
 
-// TODO customSortと sort.stable x 2をbenchで比較
+func TestCompareSort(t *testing.T) {
+	start := time.Now()
+	sort.Sort(byArtist(tracks))
+	sort.Sort(byYear(tracks))
+	a := fmt.Sprintf("%v", tracks)
+	fmt.Printf("elapsed time: %gs\n", time.Since(start).Seconds())
 
-type customSort2 struct {
-	t    []*Track
-	less func(x, y *Track) bool
-}
+	start = time.Now()
+	sort.Stable(byArtist(tracks))
+	sort.Stable(byYear(tracks))
+	b := fmt.Sprintf("%v", tracks)
+	fmt.Printf("elapsed time: %gs\n", time.Since(start).Seconds())
 
-func (x customSort2) Len() int           { return len(x.t) }
-func (x customSort2) Less(i, j int) bool { return x.less(x.t[i], x.t[j]) }
-func (x customSort2) Swap(i, j int)      { x.t[i], x.t[j] = x.t[j], x.t[i] }
-
-func BenchmarkSort(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		sort.Sort(customSort2{tracks, func(x, y *Track) bool {
-			if x.Artist != y.Artist {
-				return x.Artist < y.Artist
-			}
-			if x.Title != y.Title {
-				return x.Title < y.Title
-			}
-			return false
-		}})
-	}
-}
-
-func BenchmarkStable(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		sort.Stable(customSort2{tracks, func(x, y *Track) bool {
-			if x.Artist != y.Artist {
-				return x.Artist < y.Artist
-			}
-			if x.Title != y.Title {
-				return x.Title < y.Title
-			}
-			return false
-		}})
+	if a != b {
+		t.Fatalf("\na: %s\nb: %s\n", a, b)
 	}
 }
