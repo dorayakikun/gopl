2c2c49c432542206a6cb88c3518eaa9dc192327f
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-08-27 22:31:53 +0900

    練習問題7.17を回答

--- ch07/ex17/main.go
+++ ch07/ex17/main.go
@@ -3,36 +3,54 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"github.com/pkg/errors"
 	"io"
+	"log"
 	"os"
 	"strings"
 )
 
 func main() {
-	dec := xml.NewDecoder(os.Stdin)
+	//f, err := os.Open("foo.xml")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	err := run(os.Stdin, os.Stdout)
+	if err != nil {
+		log.Fatal()
+	}
+}
+
+func run(r io.Reader, w io.Writer) error {
+	dec := xml.NewDecoder(r)
 	var stack []string // stack of element names
 	for {
 		tok, err := dec.Token()
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			fmt.Fprintf(os.Stderr, "xmlselect: %v\n", err)
-			os.Exit(1)
+			return errors.Wrap(err, "xml decode failed")
 		}
 		switch tok := tok.(type) {
 		case xml.StartElement:
 			stack = append(stack, tok.Name.Local) // push
 			for _, a := range tok.Attr {
-				fmt.Printf("%s %s\n", a.Name.Local, a.Value)
+				if a.Name.Local == "id" {
+					stack = append(stack, "#" + a.Value)
+				}
+				if a.Name.Local == "class" {
+					stack = append(stack, "." + a.Value)
+				}
 			}
 		case xml.EndElement:
 			stack = stack[:len(stack)-1] // pop
 		case xml.CharData:
 			if containsAll(stack, os.Args[1:]) {
-				fmt.Printf("%s: %s\n", strings.Join(stack, " "), tok)
+				fmt.Fprintf(w, "%s: %s\n", strings.Join(stack, " "), tok)
 			}
 		}
 	}
+	return nil
 }
 
 func containsAll(x, y []string) bool {
