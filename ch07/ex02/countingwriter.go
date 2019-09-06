e53fe8d5b7b93db175d2fdd6722bbcaf72371443
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-09-06 23:01:35 +0900

    fix

--- ch07/ex02/countingwriter.go
+++ ch07/ex02/countingwriter.go
@@ -5,17 +5,17 @@ import (
 )
 
 type ReadCounter struct {
-	w       io.Writer
+	io.Writer
 	written int64
 }
 
 func (r *ReadCounter) Write(p []byte) (n int, err error) {
-	n, err = r.w.Write(p)
+	n, err = r.Writer.Write(p)
 	r.written += int64(n)
 	return
 }
 
 func CountingWriter(w io.Writer) (io.Writer, *int64) {
-	rc := ReadCounter{w: w, written: 0}
+	rc := ReadCounter{w, 0}
 	return &rc, &rc.written
 }
