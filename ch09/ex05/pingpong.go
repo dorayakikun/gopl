daa9e7d852b763788f4ec9677510d4e4e86834b7
Tomohide Takao <github@dorayakikun.com>
2019-09-21 16:00:39 +0900

    練習問題9.5を回答

--- ch09/ex05/pingpong.go
+++ ch09/ex05/pingpong.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+var counter int
+
 func main() {
 
+	ping := make(chan struct{})
+	pong := make(chan struct{})
+
+	go sendPing(ping, pong)
+	go sendPong(ping, pong)
+
+	ping <- struct{}{}
+
+	for {
+		time.Sleep(time.Duration(time.Second))
+		fmt.Printf("counter: %d\n", counter)
+	}
+}
+
+func sendPing(ping <-chan struct{}, pong chan<- struct{}) {
+	for {
+		<-ping
+		fmt.Println("ping")
+		pong <- struct{}{}
+	}
+}
+
+func sendPong(ping chan<- struct{}, pong <-chan struct{}) {
+	for {
+		<-pong
+		fmt.Println("pong")
+		counter++
+		ping <- struct{}{}
+	}
 }
