f5e6679f9b3d206f9a427423747a3d8145d7cabe
Tomohide Takao <github@dorayakikun.com>
2019-09-22 14:25:07 +0900

    練習問題9.3を回答

---
+++ ch09/ex03/memotest/memotest.go
@@ -0,0 +1,108 @@
+package memotest
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+	"testing"
+	"time"
+)
+
+//!+httpRequestBody
+func httpGetBody(url string, done <-chan struct{}) (interface{}, error) {
+	select {
+	case <-done:
+		return nil, errors.New(fmt.Sprintf("cancel request: %q", url))
+	default:
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		return ioutil.ReadAll(resp.Body)
+	}
+}
+
+//!-httpRequestBody
+
+var HTTPGetBody = httpGetBody
+
+func incomingURLs() <-chan string {
+	ch := make(chan string)
+	go func() {
+		for _, url := range []string{
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"http://gopl.io",
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"http://gopl.io",
+		} {
+			ch <- url
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+type M interface {
+	Get(key string, done <-chan struct{}) (interface{}, error)
+}
+
+/*
+//!+seq
+	m := memo.New(httpGetBody)
+//!-seq
+*/
+
+func Sequential(t *testing.T, m M) {
+	done := make(chan struct{})
+	close(done)
+	//!+seq
+	for url := range incomingURLs() {
+		start := time.Now()
+		value, err := m.Get(url, done)
+		//done <- struct{}{}
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Printf("%s, %s, %d bytes\n",
+			url, time.Since(start), len(value.([]byte)))
+	}
+	//!-seq
+}
+
+/*
+//!+conc
+	m := memo.New(httpGetBody)
+//!-conc
+*/
+
+func Concurrent(t *testing.T, m M) {
+	done := make(chan struct{})
+	close(done)
+	//!+conc
+	var n sync.WaitGroup
+	for url := range incomingURLs() {
+		n.Add(1)
+		go func(url string) {
+			defer n.Done()
+			start := time.Now()
+			value, err := m.Get(url, done)
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			fmt.Printf("%s, %s, %d bytes\n",
+				url, time.Since(start), len(value.([]byte)))
+		}(url)
+	}
+	n.Wait()
+	//!-conc
+}
