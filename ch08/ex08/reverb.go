e41471dac35b762636ca10c864f3bf4a7478a230
Tomohide Takao <github@dorayakikun.com>
2019-09-24 19:24:42 +0900

    練習問題8.8を回答

---
+++ ch08/ex08/reverb.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	keep := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-keep:
+				continue
+			case <-time.After(10 * time.Second):
+				fmt.Println("connection is timeout")
+				c.Close()
+				return
+			}
+		}
+	}()
+	for input.Scan() {
+		keep <- struct{}{}
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
