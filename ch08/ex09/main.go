889547c2c4bef61f3831f7d3c4aaf04e3705f0d1
Tomohide Takao <github@dorayakikun.com>
2019-09-26 21:59:31 +0900

    wip

--- ch08/ex09/main.go
+++ ch08/ex09/main.go
@@ -1,14 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 250.
-
-// The du3 command computes the disk usage of the files in a directory.
 package main
-
-// The du3 variant traverses all directories in parallel.
-// It uses a concurrency-limiting counting semaphore
-// to avoid opening too many files at once.
 
 import (
 	"flag"
@@ -22,21 +12,14 @@ import (
 
 var vFlag = flag.Bool("v", false, "show verbose progress messages")
 
-//!+
 func main() {
-	// ...determine roots...
-
-	//!-
 	flag.Parse()
 
-	// Determine the initial directories.
 	roots := flag.Args()
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
 
-	//!+
-	// Traverse each root of the file tree in parallel.
 	fileSizes := make(chan int64)
 	var n sync.WaitGroup
 	for _, root := range roots {
@@ -47,9 +30,7 @@ func main() {
 		n.Wait()
 		close(fileSizes)
 	}()
-	//!-
 
-	// Print the results periodically.
 	var tick <-chan time.Time
 	if *vFlag {
 		tick = time.Tick(500 * time.Millisecond)
@@ -60,7 +41,7 @@ loop:
 		select {
 		case size, ok := <-fileSizes:
 			if !ok {
-				break loop // fileSizes was closed
+				break loop
 			}
 			nfiles++
 			nbytes += size
@@ -69,20 +50,13 @@ loop:
 		}
 	}
 
-	printDiskUsage(nfiles, nbytes) // final totals
-	//!+
-	// ...select loop...
+	printDiskUsage(nfiles, nbytes)
 }
-
-//!-
 
 func printDiskUsage(nfiles, nbytes int64) {
 	fmt.Printf("%d files  %.1f GB\n", nfiles, float64(nbytes)/1e9)
 }
 
-// walkDir recursively walks the file tree rooted at dir
-// and sends the size of each found file on fileSizes.
-//!+walkDir
 func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 	defer n.Done()
 	for _, entry := range dirents(dir) {
@@ -96,18 +70,11 @@ func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 	}
 }
 
-//!-walkDir
-
-//!+sema
-// sema is a counting semaphore for limiting concurrency in dirents.
 var sema = make(chan struct{}, 20)
 
-// dirents returns the entries of directory dir.
 func dirents(dir string) []os.FileInfo {
-	sema <- struct{}{}        // acquire token
-	defer func() { <-sema }() // release token
-	// ...
-	//!-sema
+	sema <- struct{}{}
+	defer func() { <-sema }()
 
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
