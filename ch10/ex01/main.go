3a1ef7ab312a0a055419ce1524ccbed9da7fb417
dorayakikun <github@dorayakikun.com>
2019-10-15 17:23:34 +0900

    練習問題10.1を回答

--- ch10/ex01/main.go
+++ ch10/ex01/main.go
@@ -1,24 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"image/gif"
 	"image/jpeg"
+	"image/png"
 	_ "image/png" // register PNG decoder
 	"io"
+	"log"
 	"os"
 )
 
 func main() {
-	if err := toJPEG(os.Stdin, os.Stdout); err != nil {
-		fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
-		os.Exit(1)
-	}
+	var format string
+	flag.StringVar(&format, "format", "jpeg", "encoding format(e.g. jpeg, gif, png)")
+	
+	flag.Parse()
 
-	if err := toGIF(os.Stdin, os.Stdout); err != nil {
-		fmt.Fprintf(os.Stderr, "gif: %v\n", err)
-		os.Exit(1)
+	switch format {
+	case "jpeg":
+		if err := toJPEG(os.Stdin, os.Stdout); err != nil {
+			fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
+			os.Exit(1)
+		}
+	case "gif":
+		if err := toGIF(os.Stdin, os.Stdout); err != nil {
+			fmt.Fprintf(os.Stderr, "gif: %v\n", err)
+			os.Exit(1)
+		}
+	case "png":
+		if err := toPNG(os.Stdin, os.Stdout); err != nil {
+			fmt.Fprintf(os.Stderr, "png: %v\n", err)
+			os.Exit(1)
+		}
+	default:
+		log.Fatalf("unsuported format: %s\n", format)
 	}
 }
 
@@ -38,4 +56,13 @@ func toGIF(in io.Reader, out io.Writer) error {
 	}
 	fmt.Fprintln(os.Stderr, "Input format =", kind)
 	return gif.Encode(out, img, &gif.Options{NumColors: 256})
+}
+
+func toPNG(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "Input format =", kind)
+	return png.Encode(out, img)
 }
