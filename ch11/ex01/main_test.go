20e5fca21e4518a633242cc36984eb4edec16f6b
Tomohide Takao <github@dorayakikun.com>
2019-10-21 13:41:46 +0900

    練習問題11.1を回答

---
+++ ch11/ex01/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+	"unicode/utf8"
+)
+
+func TestRun(t *testing.T) {
+	tests := []struct {
+		in      string
+		counts  map[rune]int
+		utflen  [utf8.UTFMax + 1]int
+		invalid int
+	}{
+		{
+			"",
+			map[rune]int{},
+			[utf8.UTFMax + 1]int{0, 0, 0, 0, 0},
+			0,
+		},
+		{
+			"apple",
+			map[rune]int{'a': 1, 'p': 2, 'l': 1, 'e': 1},
+			[utf8.UTFMax + 1]int{0, 5, 0, 0, 0},
+			0,
+		},
+		{
+			"apple banana",
+			map[rune]int{'a': 4, 'b': 1, 'n': 2, 'p': 2, 'l': 1, 'e': 1, ' ': 1},
+			[utf8.UTFMax + 1]int{0, 12, 0, 0, 0},
+			0,
+		},
+	}
+
+	for _, test := range tests {
+		r := strings.NewReader(test.in)
+		counts, utflen, invalid := run(r)
+
+		if !reflect.DeepEqual(counts, test.counts) {
+			t.Errorf("counts: %v want: %v", counts, test.counts)
+		}
+
+		if !reflect.DeepEqual(utflen, test.utflen) {
+			t.Errorf("utflen: %v want: %v", utflen, test.utflen)
+		}
+
+		if invalid != test.invalid {
+			t.Errorf("invalid: %v want: %v", invalid, test.invalid)
+		}
+	}
+}
