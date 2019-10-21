f365bae70e9abe7be593ebbcfd39168dad0140b3
Tomohide Takao <github@dorayakikun.com>
2019-10-21 14:24:16 +0900

    練習問題11.2を回答

---
+++ ch11/ex02/intset_test.go
@@ -0,0 +1,82 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package intset
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Example_one() {
+	//!+main
+	var x, y IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String()) // "{1 9 144}"
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String()) // "{9 42}"
+
+	x.UnionWith(&y)
+	fmt.Println(x.String()) // "{1 9 42 144}"
+
+	fmt.Println(x.Has(9), x.Has(123)) // "true false"
+	//!-main
+
+	// Output:
+	// {1 9 144}
+	// {9 42}
+	// {1 9 42 144}
+	// true false
+}
+
+func Example_two() {
+	var x IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	x.Add(42)
+
+	//!+note
+	fmt.Println(&x)         // "{1 9 42 144}"
+	fmt.Println(x.String()) // "{1 9 42 144}"
+	fmt.Println(x)          // "{[4398046511618 0 65536]}"
+	//!-note
+
+	// Output:
+	// {1 9 42 144}
+	// {1 9 42 144}
+	// {[4398046511618 0 65536]}
+}
+
+func TestIntSet_Add(t *testing.T) {
+	tests := []struct {
+		ints []int
+	}{
+		{
+			[]int{},
+		},
+		{
+			[]int{1, 9, 42, 144},
+		},
+	}
+
+	var x IntSet
+	y := make(map[int]bool)
+
+	for _, test := range tests {
+		for _, i := range test.ints {
+			x.Add(i)
+			y[i] = true
+		}
+
+		for _, i := range test.ints {
+			if x.Has(i) != y[i] {
+				t.Errorf("unmatch x: %s y: %v", x.String(), y)
+			}
+		}
+	}
+}
