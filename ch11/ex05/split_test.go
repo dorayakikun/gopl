a55280d1276d563fdeabe075361841334ce20a6f
Tomohide Takao <github@dorayakikun.com>
2019-11-03 23:36:03 +0900

    練習問題11.5を回答

--- ch11/ex05/split_test.go
+++ ch11/ex05/split_test.go
@@ -13,9 +13,32 @@ func assertEqual(x, y int) {
 }
 
 func TestSplit(t *testing.T) {
-	s, sep := "a:b:c", ":"
-	words := strings.Split(s, sep)
-	if got, want := len(words), 3; got != want {
-		t.Errorf("Split(%q, %q) returned %d words, want %d", s, sep, got, want)
+	tests := []struct{
+		s string
+		sep string
+		want int
+	}{
+		{
+			"a:b:c",
+			":",
+			3,
+		},
+		{
+			"い、ろ、は、に、ほ、へ、と",
+			"、",
+			7,
+		},
+		{
+			"xyz",
+			",",
+			1,
+		},
+	}
+
+	for _, test := range tests {
+		words := strings.Split(test.s, test.sep)
+		if got := len(words); got != test.want {
+			t.Errorf("Split(%q, %q) returned %d words, want %d", test.s, test.sep, got, test.want)
+		}
 	}
 }
