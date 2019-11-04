244b5c89fcc823871fdc593139509730ee8a7ce5
Tomohide Takao <github@dorayakikun.com>
2019-11-04 22:57:02 +0900

    wip

---
+++ ch11/ex06/popcount.go
@@ -0,0 +1,28 @@
+package popcount
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	var ret int
+	for i := uint64(0); i < 64; i++ {
+		// 最下位ビットを比べる
+		ret += int(x & 1)
+		x >>= 1
+	}
+	return ret
+}
+
+func PopCount2(x uint64) int {
+	var ret int
+	for x > 0 {
+		x = x & (x - 1)
+		ret++
+	}
+	return ret
+}
