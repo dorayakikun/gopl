55bcab2f3cabc02eb4b0cd07ed5633b4c9eb9121
Tomohide Takao <github@dorayakikun.com>
2019-11-05 22:38:05 +0900

    練習問題11.6を回答

--- ch11/ex06/popcount_test.go
+++ ch11/ex06/popcount_test.go
@@ -1,2 +1,19 @@
 package popcount
 
+import "testing"
+
+var input uint64 = 0x1234567890ABCDEF
+
+var out int
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		out = PopCount(input)
+	}
+}
+
+func BenchmarkPopCount2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		out = PopCount2(input)
+	}
+}
